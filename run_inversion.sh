bad0aa87045663f3de5d7e075222559268c44244
mark_yu <51077410+MarcYugo@users.noreply.github.com>
2024-08-14 01:52:28 +0800

    Delete run_inversion.sh

--- run_inversion.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-export GIT_PYTHON_REFRESH=quiet
-export HUGGINGFACE_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxx
-
-# inversion 
-python preprocess.py \
-  --config_path ./configs/config_pnp_tennis.yaml
